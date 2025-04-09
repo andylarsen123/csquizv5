e972db41b513c14731bc391fa9dcdd4575295133
andylarsen123 <andyr.larsen@gmail.com>
2025-04-09 10:57:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -370,7 +370,7 @@ function showResults() {
         
         // Create a results header
         const resultsHeader = document.createElement('h2');
-        resultsHeader.textContent = "<small>Note: At the moment, only the links to Shoreline Setback, Shoreline District, Bluff Protection Overlay, Land Division, and PUDs are functional.)";
+        resultsHeader.textContent = "<small>Note: At the moment, only the links to Shoreline Setback, Shoreline District, Bluff Protection Overlay, Land Division, and PUDs are functional.)"</small> ;
         resultsHeader.style.textAlign = 'center';
         resultsHeader.style.marginBottom = '13px';
         answersList.appendChild(resultsHeader);
